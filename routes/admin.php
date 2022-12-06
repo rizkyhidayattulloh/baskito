deab710f475482852e55c9cf92a2bc7d3c5f2377
Aryan Jaya <aryanjaya88@gmail.com>
2022-12-06 17:42:49 +0800

    add login page

---
+++ routes/admin.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('login', function () {
+    return inertia('admin.auth.login');
+})->name('login');
+
+Route::get('login-2', function () {
+    return inertia('admin.auth.login-2');
+})->name('login');
