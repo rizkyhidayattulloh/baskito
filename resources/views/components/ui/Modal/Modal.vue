825a6d897d6cc76bc4bdffaa231884c3820f87dd
Adrian Edy <adrianedypratama@gmail.com>
2023-01-12 11:08:54 +0800

    add modal component

---
+++ resources/views/components/ui/Modal/Modal.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="modal fade" tabindex="-1" role="dialog" style="display: none;" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header" v-if="$slots.header">
+          <slot name="header"></slot>
+        </div>
+        <div class="modal-header" v-else>
+          <h5 class="modal-title">
+              {{ title }}
+          </h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">×</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <slot></slot>
+        </div>
+        <div class="modal-footer bg-whitesmoke br" v-if="$slots.footer">
+            <slot name="footer"></slot>
+        </div>
+        <div class="modal-footer bg-whitesmoke br" v-else>
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">{{ closeText }}</button>
+            <button type="button" class="btn btn-primary"  @click="$emit('submit')">{{ submitText }}</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  withDefaults(
+    defineProps<{
+      title?: string
+      closeText?: string
+      submitText?: string
+    }>(), {
+      closeText: () => "Close",
+      submitText: () => "Save Changes"
+    }
+  );
+</script>
--- resources/views/pages/admin/dashboard/index.vue
+++ resources/views/pages/admin/dashboard/index.vue
@@ -5,13 +5,69 @@
     <PageTitle>Dashboard</PageTitle>
     <PageDescription>Description for the page.</PageDescription>
   </PageSection>
+
+  <div class="row">
+    <div class="col-12 col-md-6 col-lg-6">
+      <div class="card">
+        <div class="card-header">
+          <h4>Modal</h4>
+        </div>
+        <div class="card-body">
+          <div class="form-group">
+            <p class="mb-2">Default Modal</p>
+            <button class="btn btn-primary" data-toggle="modal" data-target="#defaultModal">Show Default Modal</button>
+          </div>
+          <div class="form-group">
+            <p class="mb-2">Custom Naming Modal</p>
+            <button class="btn btn-primary" data-toggle="modal" data-target="#customNamingModal">Show Custom Naming Modal</button>
+          </div>
+          <div class="form-group">
+            <p class="mb-2">Custom Template Modal</p>
+            <button class="btn btn-primary" data-toggle="modal" data-target="#customTemplateModal">Show Custom Template Modal</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  <Teleport to="body">
+    <Modal title="Default Modal" id="defaultModal" @submit="submit">
+      <p>Modal body goes here.</p>
+    </Modal>
+
+    <Modal title="Custom Naming Modal" id="customNamingModal" submitText="Save" closeText="Exit" @submit="submit">
+      <p>Modal body goes here.</p>
+    </Modal>
+
+    <Modal id="customTemplateModal">
+      <template #header>
+        <b><i>Custom Template</i></b>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">×</span>
+        </button>
+      </template>
+
+      <template #default>
+        <p>Modal body goes here.</p>
+      </template>
+
+      <template #footer>
+        <button type="button" class="btn btn-warning" @click="submit">Update</button>
+        <button type="button" class="btn btn-danger">Delete</button>
+      </template>
+    </Modal>
+  </Teleport>
 </template>
 
 <script setup lang="ts">
 import PageDescription from "@components/admin/layout/Page/PageDescription.vue";
 import PageSection from "@components/admin/layout/Page/PageSection.vue";
 import PageTitle from "@components/admin/layout/Page/PageTitle.vue";
+import Modal from "@components/ui/Modal/Modal.vue";
 import { Head } from "@inertiajs/inertia-vue3";
-</script>
 
+function submit() {
+  console.log('submitted');
+}
+</script>
 <style scoped></style>
