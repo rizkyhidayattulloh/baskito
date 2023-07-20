005bc68ccf23b42f00622e79a19facad7aa8883f
Aryan Jaya <aryanjaya88@gmail.com>
2023-07-20 17:13:17 +0800

    add login function

--- routes/admin.php
+++ routes/admin.php
@@ -3,19 +3,20 @@
 use App\Http\Controllers\Admin\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('login', function () {
-    return inertia('admin.auth.login');
-})->name('login');
-
-Route::get('login-2', function () {
+Route::get('login-2', function () { // remove this route if you don't need it
     return inertia('admin.auth.login-2');
 })->name('login-2');
 
+Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('login', [LoginController::class, 'login']);
-Route::delete('logout', [LoginController::class, 'logout'])->name('logout');
 
-Route::get('/', function () {
-    return inertia('admin.dashboard.index');
-})->name('dashboard');
+Route::middleware(['auth'])->group(function () {
+    Route::delete('logout', [LoginController::class, 'logout'])->name('logout');
 
-require_once __DIR__.'/component.php';
+    Route::get('/', function () {
+        return inertia('admin.dashboard.index');
+    })->name('dashboard');
+
+    // remove this route if you don't need it
+    require_once __DIR__.'/component.php';
+});
