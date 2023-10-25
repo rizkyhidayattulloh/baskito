c345e874b675d344f6d2b4a75a34ee5046ab4ed7
Aryan Jaya <aryanjaya88@gmail.com>
2023-10-25 11:08:55 +0800

    add safelist for bootstrap class to purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -14,6 +14,37 @@ module.exports = {
         "./resources/scripts/**/*.js",
         "./resources/scripts/**/*.ts",
       ],
+      defaultExtractor(content) {
+        const contentWithoutStyleBlocks = content.replace(
+          /<style[^]+?<\/style>/gi,
+          "",
+        );
+        return (
+          contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) ||
+          []
+        );
+      },
+      safelist: [
+        /-(leave|enter|appear)(|-(to|from|active))$/,
+        /^(?!(|.*?:)cursor-move).+-move$/,
+        /^router-link(|-exact)-active$/,
+        /data-v-.*/,
+
+        // ignore bootstrap classes with this prefix from being purged
+        // it will solve the problem of missing nested classes
+        // add more prefixes if you found any missing classes
+        // /(bg).*/,
+        // /(btn).*/,
+        // /(col).*/,
+        // /(text).*/,
+        // /(border).*/,
+        // /(d-).*/,
+        /(nav).*/,
+        /(table).*/,
+        /(alert).*/,
+        /(card).*/,
+        /(modal).*/,
+      ],
     }),
   ],
 };
