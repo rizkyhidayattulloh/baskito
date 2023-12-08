49069d457ad3c7f25ad082aedab48a7d73848879
Aryan Jaya <aryanjaya88@gmail.com>
2023-12-08 11:45:54 +0800

    refactor type for route function in vue
    it's because ziggy js has their own type since version 1.8.0

--- resources/scripts/utils/ziggy/type.ts
+++ resources/scripts/utils/ziggy/type.ts
@@ -1,29 +1,10 @@
 declare module "ziggy-js";
 
-import { Config, RouteParamsWithQueryOverload, RouteParam, Router } from "ziggy-js";
-import { Ziggy } from ".";
+import routeFn, { Config } from "ziggy-js";
 
-export type RouteName = keyof (typeof Ziggy)['routes'];
-
-declare function route(
-  name: RouteName,
-  params?: RouteParamsWithQueryOverload | RouteParam,
-  absolute?: boolean,
-  config?: Config,
-): string;
-
-declare function route(
-  name?: undefined,
-  params?: RouteParamsWithQueryOverload | RouteParam,
-  absolute?: boolean,
-  config?: Config,
-): Router;
-
-export type ZiggyRoute = typeof route;
-
-declare module '@vue/runtime-core' {
+declare module "@vue/runtime-core" {
   interface ComponentCustomProperties {
-    $route: ZiggyRoute;
+    $route: typeof routeFn;
   }
 }
 
