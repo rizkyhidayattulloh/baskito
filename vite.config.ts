6a15f4cd5f3705dfb93310f626be4a0197ac2648
Aryan Jaya <aryanjaya88@gmail.com>
2023-12-22 10:54:45 +0800

    Add Ziggy TypeGenerator command
    it will resolve the problem when vite generate route & type for ziggy in same time

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,34 @@
-import { defineConfig, splitVendorChunkPlugin } from 'vite'
-import laravel, { callArtisan, findPhpPath } from 'vite-plugin-laravel'
-import vue from '@vitejs/plugin-vue'
-import inertia from './resources/scripts/vite/inertia-layout'
+import { defineConfig, splitVendorChunkPlugin } from "vite";
+import laravel, { callArtisan, findPhpPath } from "vite-plugin-laravel";
+import vue from "@vitejs/plugin-vue";
+import inertia from "./resources/scripts/vite/inertia-layout";
 
 export default defineConfig({
-	plugins: [
-		inertia(),
-		vue(),
-		laravel({
+  plugins: [
+    inertia(),
+    vue(),
+    laravel({
       watch: [
         {
           condition: (file) => file.includes("routes/"),
-          handle: () =>
+          handle: () => {
+            // generate route
             callArtisan(
               findPhpPath(),
               "ziggy:generate",
-              "resources/scripts/utils/ziggy/index.ts"
-            ),
+              "resources/scripts/utils/ziggy/index.ts",
+            );
+
+            // generate types
+            callArtisan(
+              findPhpPath(),
+              "ziggy:type",
+              "resources/scripts/types/ziggy.d.ts",
+            );
+          },
         },
       ],
     }),
     splitVendorChunkPlugin(),
-	],
-})
+  ],
+});
